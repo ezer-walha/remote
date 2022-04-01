bda9aea21c71e719a7d6e926e02ca3fcadbdc448
ezer walha <walhaezer99@gmail.com>
2022-04-01 23:42:42 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JobFormComponent } from './component/job-form/job-form.component';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobFormComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
